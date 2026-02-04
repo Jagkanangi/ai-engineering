5900127849e43d45076295bc40bf5bfa230d56f6
Jag Kanangi <jagkanangi@gmail.com>
2026-02-04 15:23:39 -0500

    include ollama

--- AI-Engineering-Essentials/ChatTwin/GradioUI.py
+++ AI-Engineering-Essentials/ChatTwin/GradioUI.py
@@ -3,8 +3,9 @@ import gradio as gr
 from llama3 import llama3
 from OpenAIModel import OpenAIModel
 llama3 = llama3()
+# function to call gardio
 def gradio_function(message, history):
     return llama3.chat(prompt=message)
 gr.ChatInterface(gradio_function).launch()
-
+ 
 #
