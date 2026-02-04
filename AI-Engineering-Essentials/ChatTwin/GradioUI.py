941b97929af31cf13d7730d21a8a6ffca2d6f6f1
Jag Kanangi <jagkanangi@gmail.com>
2026-02-04 15:07:21 -0500

    Don't instantiate llama for every call

--- AI-Engineering-Essentials/ChatTwin/GradioUI.py
+++ AI-Engineering-Essentials/ChatTwin/GradioUI.py
@@ -2,8 +2,9 @@ import gradio as gr
 
 from llama3 import llama3
 from OpenAIModel import OpenAIModel
+llama3 = llama3()
 def gradio_function(message, history):
-    return llama3().chat(message)
+    return llama3.chat(prompt=message)
 gr.ChatInterface(gradio_function).launch()
 
 #
